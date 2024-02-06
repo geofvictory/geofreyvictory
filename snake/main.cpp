02106ba46e3a04efe23f79633f1919292145b0ca
Geofrey Victory <48102629+geofvictory@users.noreply.github.com>
2024-02-06 12:29:14 -0800

    Adding start game and borders

--- snake/main.cpp
+++ snake/main.cpp
@@ -11,10 +11,22 @@ eDirecton dir;
 
 void Setup() {
     GameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruit = x rand() % width;
+    fruit = y rand() % height;
+    score = 0;
 }
 
 void Draw() {
+    system("cls");
+    for (int i = 0; i < width; i++)
+        cout << "#";
+    cout << endl;
 
+    for (int i = 0; i < width; i++)
+        cout << "#";
 }
 
 void Input() {
