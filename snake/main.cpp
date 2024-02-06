8913d9b2c6083f1b91556919b05efd71f2f7ceaa
geofvictory <48102629+geofvictory@users.noreply.github.com>
2024-02-06 09:50:39 -0800

    file cleanup and starting snake game

---
+++ snake/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// FUNCTIONS
+
+void Setup() {
+
+}
+
+void Draw() {
+
+}
+
+void Input() {
+
+}
+
+void Logic() {
+
+}
+
+// MAIN
+
+int main() {
+    
+    return 0;
+}
