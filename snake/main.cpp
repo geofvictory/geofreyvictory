3de32f0c4c915e591ad290183cd74ac10efdf7cf
Geofrey Victory <48102629+geofvictory@users.noreply.github.com>
2024-02-07 08:35:49 -0800

    Added characters for the borders

--- snake/main.cpp
+++ snake/main.cpp
@@ -21,12 +21,30 @@ void Setup() {
 
 void Draw() {
     system("cls");
+    //border
     for (int i = 0; i < width; i++)
         cout << "#";
     cout << endl;
 
-    for (int i = 0; i < width; i++)
+    for (int = 0, i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (j == 0)
+                    cout << "#";
+                
+                    cout << " ";
+
+                if (j == width -1)
+                    cout << "#";
+            }
+            cout << endl;
+        }
+
+    //border
+    for (int i = 0; i < width+2; i++)
         cout << "#";
+    cout << endl;
 }
 
 void Input() {
